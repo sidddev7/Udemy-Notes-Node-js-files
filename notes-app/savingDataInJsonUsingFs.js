db1ef8030822fcfa05350cfe7383aa5a794e078e
Siddhrajsinh Ranjitsinh Gohil <61483081+sidddev7@users.noreply.github.com>
2023-07-21 05:20:48 +0000

    21-07-23, 10:50 Am

--- notes-app/savingDataInJsonUsingFs.js
+++ notes-app/savingDataInJsonUsingFs.js
@@ -1,3 +1,4 @@
+const { default: chalk } = require("chalk");
 const fs = require("fs");
 
 const book = {
@@ -7,7 +8,7 @@ const book = {
 
 const dataStringified = JSON.stringify(book);
 
-// fs.writeFileSync("1.json", dataStringified);
+fs.writeFileSync("1.json", dataStringified);
 
 const dataBuffer = fs.readFileSync("./1.json"); //returns a buffer object
 const dataJson = dataBuffer.toString(); //converts the buffer to string
@@ -19,10 +20,22 @@ const temp = {
   livesIn: "Surat",
   position: "Developer",
 };
+fs.copyFile("./1.json", "1-copy.json", (err) => {  // here the content of copyfile will be old data
+  if (err) {
+    console.log(chalk.red(err))
+  }
+  console.log("Success")
+});
 fs.writeFile("./1.json", JSON.stringify(temp), () => {
   console.log("Updated the file successfully");
   const newBuffer = fs.readFileSync("./1.json");
   console.log(JSON.parse(newBuffer.toString()));
 });
 
-fs.copyFile("./1.json", "1-copy.json");
+// ### if you use copy file here, it will be empty, becuase the write file will trucate the original content first, and then the copyfile will see an empty file, 
+// fs.copyFile("./1.json", "1-copy.json", (err) => { //File will be empty here
+//   if (err) {
+//     console.log(chalk.red(err))
+//   }
+//   console.log("Success")
+// });
